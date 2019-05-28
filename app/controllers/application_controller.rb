73dbdced816c0ab7a7bbf5ef5e0ffb86b1fab7f6
Benjamin Baranger <baranger.benjamin@gmail.com>
2019-05-28 10:03:41 +0100

    background jobs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+
+  def update
+    if current_user.update(user_params)
+      if current_user.admin?
+        UpdateUserJob.perform_later(current_user.id)  # <- The job is queued
+      end
+      flash[:notice] = "Your profile has been updated"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
+
 end
